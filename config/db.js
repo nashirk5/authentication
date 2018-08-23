06e0ac777ff0de94204d66f3100bacc009c9d115
Nashiruddin <nashir.k@credenceis.com>
2018-08-24 00:53:04 +0530

    Connected to the mlab database.

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const dburl = 'mongodb://localhost:27017/authe';
-//const dburl = 'mongodb://nashirk5:nashirk5@ds225492.mlab.com:25492/authe';
+//const dburl = 'mongodb://localhost:27017/authe';
+const dburl = 'mongodb://nashirk5:nashirk5@ds225492.mlab.com:25492/authe';
 
 mongoose.connect(dburl, (err) => {
     if(err){
